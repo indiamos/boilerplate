2dfa40a69c1526285f1f89880ea36167d27d7cb3
Maggy Prajapati <mupraj10@gmail.com>
2017-07-24 16:24:34 -0400

    almost done with auth

---
+++ db/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./user');
+const Product = require('./product');
+
+//add associations here 
+
+module.exports = {User,Product};
---
+++ server/auth/index.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+
+router.post('/login', (req, res, next) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(user => {
+      if (!user) res.status(401).send('User not found');
+      else if (!user.hasMatchingPassword(req.body.password)) res.status(401).send('Incorrect password');
+      else {
+        req.login(user, err => {
+          if (err) next(err);
+          else res.json(user);
+        });
+      }
+    })
+    .catch(next);
+});
+
+router.post('/signup', (req, res, next) => {
+  User.create(req.body)
+    .then(user => {
+      req.login(user, err => {
+        if (err) next(err);
+        else res.json(user);
+      });
+    })
+    .catch(next);
+});
+
+router.post('/logout', (req, res, next) => {
+  req.logout();
+  res.sendStatus(200);
+});
+
+module.exports = router;
